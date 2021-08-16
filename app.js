010ca95f275bb74b967eacef6d70894780d3b093
RyanSafa <ryansafa2@gmail.com>
2021-08-15 21:53:30 -0500

    navbar

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const app = express();
 const port = 3000;
 const __dirname = path.resolve();
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs' );
+app.engine('ejs', ejsMate);
 
 app.get('/', (req,res) => {
     res.render('home')
